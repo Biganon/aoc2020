edd5113fbe0a2c07d723933f6235558bffebdbf2
Simon Junod <sj@simonjunod.ch>
2020-12-15 06:42:11 +0100

    Day 15 done

---
+++ 15/15.2.py
@@ -0,0 +1,18 @@
+numbers = [18,8,0,5,4,1,20]
+#numbers = [0, 3, 6]
+last_appearance = dict((x, numbers.index(x)+1) for x in numbers)
+
+turn = len(numbers)
+while True:
+    turn += 1
+    previous = numbers[-1]
+    if previous not in last_appearance:
+        last_appearance[previous] = turn-1
+        numbers.append(0)
+    else:
+        delta = (turn-1) - last_appearance[previous]
+        last_appearance[previous] = turn-1
+        numbers.append(delta)
+    if turn == 30000000:
+        print(numbers[-1])
+        break
