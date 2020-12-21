fd918ef0b0feaefa4d11c8eb763e7ffedda66858
Simon Junod <sj@simonjunod.ch>
2020-12-21 08:43:27 +0100

    Day 21 done

---
+++ 21/21.2.py
@@ -0,0 +1,44 @@
+import re
+from itertools import combinations
+from collections import defaultdict
+
+with open("input", "r") as f:
+    lines = f.read().splitlines()
+
+foods = []
+ingredients = set()
+
+for line in lines:
+    i = re.search(r"(.*) \(", line).group(1)
+    i = set(i.split(" "))
+    ingredients.update(i)
+    a = re.search(r"\(contains (.*)\)", line).group(1)
+    a = set(a.split(", "))
+    foods.append([i, a])
+
+#######
+
+bad_ingredients = set()
+
+while True:
+    contaminations = defaultdict(lambda: ingredients)
+
+    pairs = combinations(foods, 2)
+    for pair in pairs:
+        a, b = pair
+        a[0] -= {x[0] for x in bad_ingredients}
+        b[0] -= {x[0] for x in bad_ingredients}
+        a[1] -= {x[1] for x in bad_ingredients}
+        b[1] -= {x[1] for x in bad_ingredients}
+        iic = a[0].intersection(b[0])
+        aic = a[1].intersection(b[1])
+        for allergen in aic:
+            contaminations[allergen] = contaminations[allergen].intersection(iic)
+
+    for (k,v) in [(k,v) for (k,v) in contaminations.items() if len(v) == 1]:
+        bad_ingredients.add(  (  list(v)[0], k  ) )
+
+    if len(contaminations) == 0:
+        break
+
+print(",".join(x[0] for x in sorted(bad_ingredients, key=lambda x:x[1])))
