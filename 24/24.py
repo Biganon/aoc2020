37b4c996f58ab9020fda68f927890efe3e45b9c5
Simon Junod <sj@simonjunod.ch>
2020-12-25 20:42:08 +0100

    Day 24 a bit faster and cleaner

--- 24/24.py
+++ 24/24.py
@@ -1,6 +1,5 @@
 from collections import defaultdict
 import re
-from copy import deepcopy
 
 tiles = defaultdict(lambda: False)
 
@@ -24,42 +23,41 @@ for line in lines:
     tuple_ = (cursor["x"], cursor["y"])
     tiles[tuple_] = not tiles[tuple_]
 
+tiles = dict(tiles) # no need for a defaultdict anymore
+
 for i in range(100):
 
-    to_test = dict((k,v) for (k,v) in tiles.items() if v)
+    to_test = dict((k,v) for (k,v) in tiles.items() if v) # remove the white tiles
 
+    # Add the white tiles surrounding the black ones:
     extras = {}
     for (x,y) in to_test.keys():
         for delta in deltas.values():
             dx, dy = delta.values()
-            if dx == 0 and dy == 0:
+            if dx == 0 and dy == 0: # the black tile itself
                 continue
-            if (x+dx, y+dy) in to_test:
+            if (x+dx, y+dy) in to_test: # another black tile
                 continue
-            if (x+dx) % 2 != (y+dy) % 2:
+            if (x+dx) % 2 != (y+dy) % 2: # an impossible tile (not all coordinates are valid)
                 continue
-            extras[(x+dx, y+dy)] = False
+            extras[(x+dx, y+dy)] = False # add it, as a white tile
     to_test.update(extras)
 
-    new = defaultdict(lambda: False)
+    new = {}
 
     for ((x,y), current) in to_test.items():
         nw = int(to_test.get((x-1, y+1), False))
-        ne = int(to_test.get((x+1, y+1), False)) 
-        sw = int(to_test.get((x-1, y-1), False)) 
-        se = int(to_test.get((x+1, y-1), False)) 
-        w  = int(to_test.get((x-2, y+0), False)) 
-        e  = int(to_test.get((x+2, y+0), False)) 
+        ne = int(to_test.get((x+1, y+1), False))
+        sw = int(to_test.get((x-1, y-1), False))
+        se = int(to_test.get((x+1, y-1), False))
+        w  = int(to_test.get((x-2, y+0), False))
+        e  = int(to_test.get((x+2, y+0), False))
         
         n = sum([nw, ne, sw, se, w, e])
 
-        if current and (n == 0 or n > 2):
-            pass
-        elif not current and n == 2:
-            new[(x, y)] = True
-        elif current:
+        if (current and 1 <= n <= 2) or (not current and n == 2):
             new[(x, y)] = True
 
-    print(f"Day {i+1}: {len([tile for tile in new.values() if tile])}")
+    print(f"Day {i+1}: {len([tile for tile in new.values() if tile])}") # count the black tiles
 
     tiles = new
